1d2743be1a6da273621f4e5be8943b37cc5a24ec
Valerii Mazurenko <Vmazurenko@gmail.com>
2022-01-18 13:21:21 +0200

    btn-accent-change-library

--- src/js/header.js
+++ src/js/header.js
@@ -1,11 +1,15 @@
 const navElemHome = document.querySelector('.nav-list__item-home');
 const navElemLibrary = document.querySelector('.nav-list__item-library');
 const searchForm = document.querySelector('.search');
-const libraryButtons = document.querySelector('.library-btns');
+const libraryButtons = document.querySelector('.library__btns');
 const headerContainer = document.querySelector('.header__container');
+const buttonWatched = document.querySelector('.library__btn-watched');
+const buttonQueue = document.querySelector('.library__btn-queue')
 
 navElemLibrary.addEventListener('click', changeClassToLibrary);
 navElemHome.addEventListener('click', changeClassToHome);
+buttonWatched.addEventListener('click', addWatchedBtnAccent);
+buttonQueue.addEventListener('click', addQueueBtnAccent);
 
 function changeClassToHome(){    
     navElemHome.classList.add('nav-list__item-home-active');
@@ -27,4 +31,13 @@ function changeClassToLibrary(e){
 
     headerContainer.classList.remove('header__container');
     headerContainer.classList.add('header__container-library');
+}
+function addWatchedBtnAccent (){
+    buttonWatched.classList.add('library__btn-active');
+    buttonQueue.classList.remove('library__btn-active');
+}
+function addQueueBtnAccent(){
+    buttonWatched.classList.remove('library__btn-active');
+    buttonWatched.classList.add('library__btn');
+    buttonQueue.classList.add('library__btn-active');
 }
