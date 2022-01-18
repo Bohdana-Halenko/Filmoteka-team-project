0c4d1c045371ac3ede65fc88a453ab0899b623a4
DrDoxi87 <Levan.Maxim@gmail.com>
2022-01-18 14:49:23 -0500

    поправил стили

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,29 @@
-import '../sass/main.scss'
+import '../sass/main.scss';
 
 import axios from 'axios';
 
 import './header';
 import './loadStartGallery';
 
-
 // Pagination
 import { getTrendingMovie, getMovieBySearch, getMovieDetails, getGenres } from './apiService';
-import { renderPaginationTrendingMovie } from "./pagination";
-
+import { renderPaginationTrendingMovie } from './pagination';
 
 getTrendingMovie()
-    .then((films) => {
-        renderPaginationTrendingMovie(films.totalTrending)
-    })
-    .catch((error) => console.error(error.message));
-
+  .then(films => {
+    renderPaginationTrendingMovie(films.totalTrending);
+  })
+  .catch(error => console.error(error.message));
 
 import './loadModalCard';
 import './convertData';
 
 import './modalFooter';
 
-
 import API from './apiService';
 
 //Loader
 import './loader';
-
 
 // Проверка работы API - это просто тест
 // API.getTrendingMovie().then(res => console.log('Trending res', res));
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,39 +1,36 @@
 import Pagination from 'tui-pagination';
-import '../sass/main.css';
-
-
 
 export function renderPaginationTrendingMovie(totalItems) {
-    const container = document.getElementById('tui-pagination-container');
-    const options = {
-        totalItems,
-        itemsPerPage: 20,
-        visiblePages: 5,
-        page: 1,
-        centerAling: false,
-        firstItemClassName: 'tui-first-child',
-        lastItemClassName: 'tui-last-child',
-        template: {
-            page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-            currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-            moveButton:
-                '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</a>',
-            disabledMoveButton:
-                '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</span>',
-            moreButton:
-                '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
-                '<span class="tui-ico-ellip">...</span>' +
-                '</a>',
-        }
-    }
+  const container = document.getElementById('tui-pagination-container');
+  const options = {
+    totalItems,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    page: 1,
+    centerAling: false,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
+    },
+  };
 
-    const pagination = new Pagination(container, options);
+  const pagination = new Pagination(container, options);
 
-    pagination.on('afterMove', function (eventData) {
-        console.log( eventData.page);
-    });
+  pagination.on('afterMove', function (eventData) {
+    console.log(eventData.page);
+  });
 }
