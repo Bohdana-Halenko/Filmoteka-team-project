395d0a7f2c0e47f06829b5825cfa8d3ee86bcedc
DrDoxi87 <Levan.Maxim@gmail.com>
2022-01-18 17:06:08 -0500

    Рендеринг галереи по клику на лого
    Реализован рендеринг галереи по клику на лого. Проблема в том, что пока не знаю куда прилепить setTimeout, слишком быстро перезагружает все

--- src/js/loadStartGallery.js
+++ src/js/loadStartGallery.js
@@ -1,21 +1,40 @@
 import gallery from '../templates/gallery.hbs';
 import API from './apiService';
-
 import transformData from './transformData';
 import transformGenres from './transfomGenres';
 
+// нашли лого
+const homeLogo = document.querySelector('.logo__link');
+
 const galleryList = document.querySelector('.gallery-list');
 
+// слушаем нажатие на logo
+homeLogo.addEventListener('click', renderClickLogo, false);
+
+// функция рендера
 function loadStartGallery(data) {
   const markup = gallery(data);
 
-  galleryList.insertAdjacentHTML('beforeend', markup);
+  // предварительно выбрана функция чистого рендера
+  // galleryList.insertAdjacentHTML('beforeend', markup);
+  galleryList.innerHTML = markup;
 }
 
-API.getTrendingMovie().then(({ resultsTrending }) => {
-  transformData(resultsTrending);
-  transformGenres(resultsTrending);
-  loadStartGallery(resultsTrending);
-});
+// функция получения данных с сервера и коррекция даты и жанров
+function dataRequest() {
+  API.getTrendingMovie().then(({ resultsTrending }) => {
+    transformData(resultsTrending);
+    transformGenres(resultsTrending);
+    loadStartGallery(resultsTrending);
+  });
+}
+// вызов функции рендера галереи при загрузке страницы
+dataRequest();
+
+// функция рендера галереи без перезагрузки страницы
+function renderClickLogo(evt) {
+  evt.preventDefault();
+  dataRequest();
+}
 
 export { loadStartGallery };
