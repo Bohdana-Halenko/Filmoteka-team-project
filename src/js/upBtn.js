2d1b97f5a77eec02a76db9d066090896965dbac3
HelgaNaz <onworkit@gmail.com>
2022-01-20 21:55:37 +0200

    добавляет плавный скролл и кнопку трейлера

--- src/js/upBtn.js
+++ src/js/upBtn.js
@@ -7,8 +7,11 @@ window.onscroll = scrollFunction;
 
 function scrollFunction() {
   upBtnEl.style.display = document.documentElement.scrollTop > 300 ? 'block' : 'none';
+  
 }
 
 upBtnEl.addEventListener('click', () => {
   document.documentElement.scrollTop = 0;
+  
 });
+
