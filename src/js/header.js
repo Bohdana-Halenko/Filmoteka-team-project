84192bc27c5a18f099953cf8d417734147148c80
Valerii Mazurenko <Vmazurenko@gmail.com>
2022-01-20 16:26:08 +0200

    change-class-sign-reg

--- src/js/header.js
+++ src/js/header.js
@@ -20,6 +20,7 @@ function changeClassToHome(e) {
   navElemLibrary.classList.remove('nav-list__item-library-active');
 
   searchForm.classList.remove('is-hidden');
+  searchForm.classList.add('search');
   libraryButtons.classList.add('is-hidden');
 
   headerContainer.classList.remove('header__container-library');
